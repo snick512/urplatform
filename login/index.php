01066ec6236321f380b4bc1b2b511c827e5a9632
snick512 <snick@isnick.net>
2020-10-17 16:08:14 -0400

    - Login UI frontend

--- login/index.php
+++ login/index.php
@@ -3,10 +3,15 @@ $userrole = 'loginpage';
 $title = 'Login';
 include 'misc/pagehead.php';
 ?>
-</head>
-<body>
   <?php require 'misc/pullnav.php'; ?>
-    <div class="container logindiv">
+  
+  <!--Main layout-->
+  <main>
+    <div class="container">
+
+      <!--Section: Main features & Quick Start-->
+      <section>
+      <div class="container logindiv">
         <div class="col-sm-4"></div>
         <div class="col-sm-4">
             <form class="text-center" name="loginform" method="post" action="ajax/checklogin.php">
@@ -30,7 +35,74 @@ include 'misc/pagehead.php';
         </div>
         <div class="col-sm-4"></div>
     </div>
-    <!-- The AJAX login script -->
-    <script src="js/login.js"></script>
+
+      </section>
+      <!--Section: More-->
+
+    </div>
+  </main>
+  <!--Main layout-->
+
+  <!--Footer-->
+  <footer class="page-footer text-center font-small mt-4 wow fadeIn">
+
+    <!--Call to action-->
+    <div class="pt-4">
+      <a class="btn btn-outline-white" href="https://urcpu.com/gate/" target="_blank"
+        role="button"><i class="fas fa-credit-card ml-2"></i> Make a payment</a>
+    </div>
+    <!--/.Call to action-->
+
+    <hr class="my-4">
+
+    <!-- Social icons -->
+    <div class="pb-4">
+      <a href="https://www.facebook.com/switchboardoffice" target="_blank">
+        <i class="fab fa-facebook-f mr-3"></i>
+      </a>
+
+      <a href="https://twitter.com/snick512" target="_blank">
+        <i class="fab fa-twitter mr-3"></i>
+      </a>
+
+      <a href="https://www.youtube.com/tyclifford" target="_blank">
+        <i class="fab fa-youtube mr-3"></i>
+      </a>
+      <a href="https://github.com/snick512" target="_blank">
+        <i class="fab fa-github mr-3"></i>
+      </a>
+
+    </div>
+    <!-- Social icons -->
+
+    <!--Copyright-->
+    <div class="footer-copyright py-3">
+      © 2020 UR CPU, Tyeme & Co., dba | +1-631-663-1317
+    </div>
+    <!--/.Copyright-->
+
+  </footer>
+  <!--/.Footer-->
+
+  <!-- SCRIPTS -->
+  <!-- JQuery -->
+
+  <script type="text/javascript" src="http://platform.urcpu.com/js/jquery-3.4.1.min.js"></script>
+  <!-- Bootstrap tooltips -->
+  <script type="text/javascript" src="http://platform.urcpu.com/js/popper.min.js"></script>
+  <!-- Bootstrap core JavaScript -->
+  <script type="text/javascript" src="http://platform.urcpu.com/js/bootstrap.min.js"></script>
+  <!-- MDB core JavaScript -->
+  <script type="text/javascript" src="http://platform.urcpu.com/js/mdb.min.js"></script>
+  <!-- Initializations -->
+  <script type="text/javascript">
+    // Animations initialization
+    new WOW().init();
+
+  </script>
+
+      <!-- The AJAX login script -->
+      <script src="js/login.js"></script>
 </body>
+
 </html>
