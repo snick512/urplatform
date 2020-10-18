42ce652a3f20f23d83cab659ae61a6aa1d449ebd
snick512 <snick@isnick.net>
2020-10-18 18:08:12 -0400

    - Meekro config

--- index.php
+++ index.php
@@ -12,13 +12,20 @@ $uruser = $_SESSION['username'];
 
 require "login/misc/pullnav.php";
 
+require_once 'login/db.class.php';
+DB::$user = 'platform';
+DB::$password = '654654654654';
+DB::$dbName = 'platform';
+$urtable = "urbase_requests"; 
+
 
 if ($auth->isLoggedIn()) {
 // $_SESSION['username']
 // logged in
 //echo "We're logged in as ".$_SESSION['username']."";
 
-include "home.php";
+//include "home.php";
+include "dashboard.php";
 
 } else {
 // not logged in
